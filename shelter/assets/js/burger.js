05c56b097dc723f5ece93e82c37bd2a0b55e6ab7
Alexey888888 <brdkmail@gmail.com>
2023-04-07 01:40:15 +0300

    feat: add burger.js for index.html (Fri, Apr 07, 2023 01:40:15 AM)

---
+++ shelter/assets/js/burger.js
@@ -0,0 +1,36 @@
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
+const blackout = document.querySelector(".blackout");
+const link = document.querySelectorAll(".menu__link");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("burger_active");
+  menu.classList.toggle("menu_active");
+  body.classList.toggle("scroll-lock");
+  blackout.classList.toggle("blackout_active");
+});
+
+link.forEach(function (el) {
+  el.addEventListener("click", () => {
+    burger.classList.remove("burger_active");
+    menu.classList.remove("menu_active");
+    body.classList.remove("scroll-lock");
+    blackout.classList.remove("blackout_active");
+  });
+});
+
+blackout.addEventListener("click", () => {
+  burger.classList.remove("burger_active");
+  menu.classList.remove("menu_active");
+  body.classList.remove("scroll-lock");
+  blackout.classList.remove("blackout_active");
+});
+
+//????????????????????????
+// menu.addEventListener("click", () => {
+//   burger.classList.remove("burger_active");
+//   menu.classList.remove("menu_active");
+//   body.classList.remove("scroll-lock");
+//   blackout.classList.remove("blackout_active");
+// });
