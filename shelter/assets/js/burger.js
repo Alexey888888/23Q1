5775174be3b629887195ea93928c62d0c3386668
Alexey888888 <brdkmail@gmail.com>
2023-04-07 11:49:21 +0300

    feat: done burger (Fri, Apr 07, 2023 11:49:21 AM)

--- shelter/assets/js/burger.js
+++ shelter/assets/js/burger.js
@@ -2,17 +2,18 @@ const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
 const body = document.querySelector("body");
 const blackout = document.querySelector(".blackout");
-const link = document.querySelectorAll(".menu__link");
+const burgerLink = document.querySelectorAll(".menu__link");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("burger_active");
   menu.classList.toggle("menu_active");
+  menu.classList.toggle("menu-pets_active");
   body.classList.toggle("scroll-lock");
   blackout.classList.toggle("blackout_active");
 });
 
-link.forEach(function (el) {
-  el.addEventListener("click", () => {
+burgerLink.forEach((link) => {
+  link.addEventListener("click", () => {
     burger.classList.remove("burger_active");
     menu.classList.remove("menu_active");
     body.classList.remove("scroll-lock");
@@ -26,11 +27,3 @@ blackout.addEventListener("click", () => {
   body.classList.remove("scroll-lock");
   blackout.classList.remove("blackout_active");
 });
-
-//????????????????????????
-// menu.addEventListener("click", () => {
-//   burger.classList.remove("burger_active");
-//   menu.classList.remove("menu_active");
-//   body.classList.remove("scroll-lock");
-//   blackout.classList.remove("blackout_active");
-// });
